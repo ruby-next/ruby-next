d77b05663cc144f1cf86f5a95972b52a4f1e676a
Vladimir Dementyev <dementiev.vm@gmail.com>
2019-11-11 21:31:06 +0300

    Fix cli specs to work with jruby

--- spec/cli/spec_helper.rb
+++ spec/cli/spec_helper.rb
@@ -5,10 +5,11 @@ require "open3"
 module CliTesting
   refine Kernel do
     def run_cli(command, opt_string = "", chdir: nil, should_fail: false)
-      output, err, status = Open3.capture3(
-        "bundle exec #{command} #{opt_string}",
-        chdir: chdir || File.expand_path("../../bin", __dir__)
-      )
+      output, err, status =
+        Open3.capture3(
+          "bundle exec #{command} #{opt_string}",
+          chdir: chdir || File.expand_path("../..", __dir__)
+        )
 
       err.should be_empty unless should_fail
       yield status, output, err
