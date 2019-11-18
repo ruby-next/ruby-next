4bca11b2ff0ca75295f7e18ae25686a83f1c99d5
Vladimir Dementyev <dementiev.vm@gmail.com>
2019-11-18 13:28:43 -0600

    Add mixed pattern matching bench

---
+++ benchmarks/pattern_matching_mixed.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+# Hack to use bundled benchmark-driver
+module Bundler
+  def self.with_unbundled_env(&block); yield; end
+end
+
+require "benchmark_driver"
+
+require "ruby-next/language"
+
+# from https://www.ruby-lang.org/en/news/2019/10/22/ruby-2-7-0-preview2-released/
+source = %q{
+def alice(data)
+  case data
+  in {name: "Alice", children: [{name: "Bob", age: age}]}
+    p "Bob age is #{age}"
+  in _
+    "No Alice"
+  end
+end
+}
+
+next_source = RubyNext::Language.transform(source).gsub! "def alice(", "def alice_next("
+
+Benchmark.driver do |x|
+  x.prelude %Q{
+    require 'json'
+
+    #{source}
+
+    #{next_source}
+
+    data = {
+      name: 'Alice',
+      children: [{
+        name: 'Bob',
+        age: 30
+      }]
+    }
+  }
+  x.report "baseline", %{ alice data }
+  x.report "transpiled", %{ alice_next data }
+end
