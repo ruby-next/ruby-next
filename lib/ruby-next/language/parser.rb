5bbd87dee0caa7a7eb40cebc115bb5664773c546
Vladimir Dementyev <dementiev.vm@gmail.com>
2019-11-20 14:31:05 -0600

    Remove parser patch

--- lib/ruby-next/language/parser.rb
+++ lib/ruby-next/language/parser.rb
@@ -10,14 +10,3 @@ Parser::Builders::Default.emit_lambda = true
 Parser::Builders::Default.emit_procarg0 = true
 Parser::Builders::Default.emit_encoding = true
 Parser::Builders::Default.emit_index = true
-
-Parser::Builders::Default.prepend(Module.new do
-  def match_hash_var_from_str(begin_t, strings, end_t)
-    super.tap do
-      string = strings[0]
-      next unless string.type == :str
-      name, = *string
-      @parser.static_env.declare(name)
-    end
-  end
-end)
