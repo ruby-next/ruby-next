d2c7fdd994e2a69313025fef03a8366181e5b12c
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-01-03 18:38:17 -0500

    Fix custom pattern matching specs

--- spec/language/pattern_matching_custom_spec.rb
+++ spec/language/pattern_matching_custom_spec.rb
@@ -4,35 +4,39 @@ using TestUnitToMspec
 
 # These tests are not copied from ruby/ruby
 describe "custom tests" do
-  # multiple clauses with arrays
-  assert_block do
-    case [0, 1, 2]
-    in [0, 2, *a]
-      false
-    in [0, *a]
-      a == [1, 2]
+  it "multiple clauses" do
+    # multiple clauses with arrays
+    assert_block do
+      case [0, 1, 2]
+      in [0, 2, *a]
+        false
+      in [0, *a]
+        a == [1, 2]
+      end
+    end
+
+    #  multiple clauses with hash
+    assert_block do
+      case {a: 0, b: 1}
+        in a: 1, **b
+          false
+        in a:, **b
+          a == 0 && b == {b: 1}
+        end
     end
   end
 
-  #  multiple clauses with hash
-  assert_block do
-    case {a: 0, b: 1}
-      in a: 1, **b
-        false
-      in a:, **b
-        a == 0 && b == {b: 1}
-      end
-  end
+  it "in operator" do
+    # in with hash
+    assert_block do
+      {a: [0, 1, 2]} in {a:}
+      a == [0, 1, 2]
+    end
 
-  # in with hash
-  assert_block do
-    {a: [0, 1, 2]} in {a:}
-    a == [0, 1, 2]
-  end
-
-  # in with hash and array rest
-  assert_block do
-    {a: [0, 1, 2]} in {a: [0, *r]}
-    r == [1, 2]
+    # in with hash and array rest
+    assert_block do
+      {a: [0, 1, 2]} in {a: [0, *r]}
+      r == [1, 2]
+    end
   end
 end
