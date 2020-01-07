483101a038a3191860ef06e414d9707f10d20386
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-01-07 17:32:04 -0500

    Use non-default parser

--- lib/ruby-next/language/unparser.rb
+++ lib/ruby-next/language/unparser.rb
@@ -6,18 +6,3 @@ require "parser/current"
 $VERBOSE = save_verbose
 
 require "unparser"
-
-# Unparser patches
-
-# Unparser doesn't support endless ranges
-# Source: https://github.com/mbj/unparser/blob/a4f959d58b660ef0630659efa5882fc20936eb18/lib/unparser/emitter/literal/range.rb
-# TODO: propose a PR
-class Unparser::Emitter::Literal::Range
-  private
-
-  def dispatch
-    visit(begin_node)
-    write(TOKENS.fetch(node.type))
-    visit(end_node) unless end_node.nil?
-  end
-end
