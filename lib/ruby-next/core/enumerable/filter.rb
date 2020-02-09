04769a526834ffe48b5d0be8ab1da0483de5e130
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-02-08 21:03:12 -0500

    Refactor core to use intermediate patch representation
    Collect patches code into Patch objects which could be used to define refinements and will be used to generate core extensions

--- lib/ruby-next/core/enumerable/filter.rb
+++ lib/ruby-next/core/enumerable/filter.rb
@@ -1,23 +1,21 @@
 # frozen_string_literal: true
 
 unless [].respond_to?(:filter)
-  RubyNext.module_eval do
-    refine Enumerable do
-      alias filter select
-    end
+  RubyNext::Core.patch Enumerable, name: "EnumerableFilter" do
+    alias filter select
+  end
 
-    # Refine Array seprately, 'cause refining modules is vulnerable to prepend:
-    # - https://bugs.ruby-lang.org/issues/13446
-    #
-    # Also, Array also have `filter!`
-    refine Array do
-      alias filter select
-      alias filter! select!
-    end
+  # Refine Array seprately, 'cause refining modules is vulnerable to prepend:
+  # - https://bugs.ruby-lang.org/issues/13446
+  #
+  # Also, Array also have `filter!`
+  RubyNext::Core.patch Array, name: "ArrayFilter" do
+    alias filter select
+    alias filter! select!
+  end
 
-    refine Hash do
-      alias filter select
-      alias filter! select!
-    end
+  RubyNext::Core.patch Hash, name: "HashFilter" do
+    alias filter select
+    alias filter! select!
   end
 end
--- lib/ruby-next/core/proc/compose.rb
+++ lib/ruby-next/core/proc/compose.rb
@@ -2,19 +2,17 @@
 
 # rubocop:disable Style/LambdaCall
 unless proc {}.respond_to?(:<<)
-  RubyNext.module_eval do
-    refine Proc do
-      def <<(other)
-        raise TypeError, "callable object is expected" unless other.respond_to?(:call)
-        this = self
-        proc { |*args, &block| this.(other.(*args, &block)) }
-      end
+  RubyNext::Core.patch Proc, name: "ProcCompose" do
+    def <<(other)
+      raise TypeError, "callable object is expected" unless other.respond_to?(:call)
+      this = self
+      proc { |*args, &block| this.(other.(*args, &block)) }
+    end
 
-      def >>(other)
-        raise TypeError, "callable object is expected" unless other.respond_to?(:call)
-        this = self
-        proc { |*args, &block| other.(this.(*args, &block)) }
-      end
+    def >>(other)
+      raise TypeError, "callable object is expected" unless other.respond_to?(:call)
+      this = self
+      proc { |*args, &block| other.(this.(*args, &block)) }
     end
   end
 end
