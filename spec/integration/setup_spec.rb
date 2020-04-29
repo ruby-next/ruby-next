294a924a1f81a8ffebe025ef4478665f4f73f12d
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-04-29 15:34:02 +0300

    Try to auto-transpile gems installed from source

--- spec/integration/setup_spec.rb
+++ spec/integration/setup_spec.rb
@@ -5,6 +5,11 @@ require_relative "../support/command_testing"
 using CommandTesting
 
 describe "setup load path" do
+  after do
+    FileUtils.rm_rf(File.join(__dir__, "fixtures", "lib", ".rbnext_test")) if
+      File.directory?(File.join(__dir__, "fixtures", "lib", ".rbnext_test"))
+  end
+
   it "loads correct file versions" do
     skip if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.7")
     run_ruby(
@@ -23,6 +28,22 @@ describe "setup load path" do
       "-e 'puts [Txen.call(\"ace\", \"ace\"), Txen.call(\"ace\", \"4\", \"5\")].join(\";\")'"
     ) do |_status, output, _err|
       output.should include("ok;ok")
+    end
+  end
+
+  it "autotranspiles files is rbnext_dir is missing" do
+    run_ruby(
+      "-I#{File.join(__dir__, "fixtures", "lib")} " \
+      "-r txen_source " \
+      "-e 'puts [Txen.call(\"ace\", \"ace\"), Txen.call(\"ace\", \"4\", \"5\")].join(\";\")'"
+    ) do |_status, output, _err|
+      output.should include("ok;ok")
+      File.directory?(File.join(__dir__, "fixtures", "lib", ".rbnext_test")).should equal true
+      # .rbnextrc is preserved
+      File.exist?(File.join(__dir__, "fixtures", "lib", ".rbnext_test", "2.7", "txen", "cards.rb")).should equal true
+      File.read(File.join(__dir__, "fixtures", "lib", ".rbnext_test", "2.7", "txen", "cards.rb")).lines.size.should equal(
+        File.read(File.join(__dir__, "fixtures", "lib", "txen", "cards.rb")).lines.size
+      )
     end
   end
 end
