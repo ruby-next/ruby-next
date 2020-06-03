7254eade29732bbca0d4c5ade837e3bfb8296958
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-06-03 21:27:45 +0300

    Make Time#ceil compatible with Ruby 2.3

--- lib/ruby-next/core/time/ceil.rb
+++ lib/ruby-next/core/time/ceil.rb
@@ -3,7 +3,8 @@
 RubyNext::Core.patch Time, method: :ceil, version: "2.7" do
   <<~'RUBY'
     def ceil(den = 0)
-      change = subsec.ceil(den) - subsec
+      sceil = (subsec * 10**den).ceil.to_r / 10**den
+      change = sceil - subsec
       self + change
     end
   RUBY
