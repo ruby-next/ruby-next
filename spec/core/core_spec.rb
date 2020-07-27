f9c1da2703834f3cdf86b7cf23fef1a94ce33625
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-07-27 12:43:32 +0300

    Remove TruffleRuby hack from core specs

--- spec/core/core_spec.rb
+++ spec/core/core_spec.rb
@@ -10,9 +10,6 @@ describe "refined patches" do
 
     activated_patches = RubyNext::Core.patches.refined.values.flatten.uniq
 
-    # See https://github.com/oracle/truffleruby/issues/2050
-    activated_patches.delete_if { _1.name == "StringSplit" } if defined?(TruffleRuby)
-
     incorrect_patches = activated_patches.select { |patch| Gem::Version.new(patch.version) <= ruby_version }
 
     incorrect_patches.map(&:name).should == []
