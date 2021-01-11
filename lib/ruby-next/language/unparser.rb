a2bca2c566dfc9ab28752275f5a4c38be7d77c89
Vladimir Dementyev <dementiev.vm@gmail.com>
2021-01-11 13:39:10 +0300

    Fix compatibility with kwargs-less unparser

--- lib/ruby-next/language/unparser.rb
+++ lib/ruby-next/language/unparser.rb
@@ -6,3 +6,8 @@ require "parser/current"
 $VERBOSE = save_verbose
 
 require "unparser"
+
+# For backward compatibility with older Unparser
+if RubyNext::Language::Builder.respond_to?(:emit_kwargs=) && !defined?(Unparser::Emitter::Kwargs)
+  RubyNext::Language::Builder.emit_kwargs = false
+end
