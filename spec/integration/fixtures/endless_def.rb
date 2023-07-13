6018dad576b375b28b51e8adfb78dbc24fda98f4
Vladimir Dementyev <palkan@hey.com>
2023-07-12 19:53:28 -0700

    style: upgrade RuboCop

--- spec/integration/fixtures/endless_def.rb
+++ spec/integration/fixtures/endless_def.rb
@@ -2,11 +2,11 @@
 
 def greet(val) =
   case val
-    in hello: hello if hello =~ /human/i
-      puts "human"
-    in hello: "martian"
-      puts "alien"
-    end
+  in hello: hello if hello =~ /human/i
+    puts "human"
+  in hello: "martian"
+    puts "alien"
+  end
 
 # p greet(hello: "Human") => 'human'
 # p greet(hello: "martian") => 'alien'
