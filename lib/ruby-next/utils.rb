f75825406f14ac209e0bc2bb9e5afbf8381a1239
Vladimir Dementyev <palkan@hey.com>
2023-07-13 20:58:17 -0700

    refactor: migrate ruby-next runtime to require-hooks

--- lib/ruby-next/utils.rb
+++ lib/ruby-next/utils.rb
@@ -4,48 +4,6 @@ module RubyNext
   module Utils
     module_function
 
-    def lookup_feature_path(path, implitic_ext: true)
-      path = "#{path}.rb" if File.extname(path).empty? && implitic_ext
-
-      # Resolve relative paths only against current directory
-      if path.match?(/^\.\.?\//)
-        path = File.expand_path(path)
-        return path if File.file?(path)
-        return nil
-      end
-
-      if Pathname.new(path).absolute?
-        path = File.expand_path(path)
-        return File.file?(path) ? path : nil
-      end
-
-      # not a relative, not an absolute path — bare path; try looking relative to current dir,
-      # if it's in the $LOAD_PATH
-      if $LOAD_PATH.include?(Dir.pwd) && File.file?(path)
-        return File.expand_path(path)
-      end
-
-      $LOAD_PATH.find do |lp|
-        lpath = File.join(lp, path)
-        return File.expand_path(lpath) if File.file?(lpath)
-      end
-    end
-
-    if $LOAD_PATH.respond_to?(:resolve_feature_path)
-      def resolve_feature_path(feature, implitic_ext: true)
-        if implitic_ext
-          $LOAD_PATH.resolve_feature_path(feature)&.last
-        else
-          lookup_feature_path(feature, implitic_ext: implitic_ext)
-        end
-      rescue LoadError
-      end
-    else
-      def resolve_feature_path(feature, implitic_ext: true)
-        lookup_feature_path(feature, implitic_ext: implitic_ext)
-      end
-    end
-
     def source_with_lines(source, path)
       source.lines.map.with_index do |line, i|
         "#{(i + 1).to_s.rjust(4)}:  #{line}"
