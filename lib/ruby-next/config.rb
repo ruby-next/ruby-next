7e411e19e0d0a22d6311c3293ec7e87901b215e3
Vladimir Dementyev <dementiev.vm@gmail.com>
2026-01-13 15:41:29 +0300

    * Ruby 4.0 is out

--- lib/ruby-next/config.rb
+++ lib/ruby-next/config.rb
@@ -10,10 +10,11 @@ module RubyNext
   # Defines last minor version for every major version
   LAST_MINOR_VERSIONS = {
     2 => 8, # 2.8 is required for backward compatibility: some gems already uses it
-    3 => 5
+    3 => 4,
+    4 => 0
   }.freeze
 
-  LATEST_VERSION = [3, 5].freeze
+  LATEST_VERSION = [4, 0].freeze
 
   # A virtual version number used for proposed features
   NEXT_VERSION = "1995.next.0"
